7fb06dc0cb31e170b8b681dec59efa3573bba849
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-08-08 19:51:59 -0300

    getting used to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
   entry: "./src/index.js",
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "TITLE",
+      template: "./src/index.html",
+    }),
+  ],
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
 };
